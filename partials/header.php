89a8ca5bcefd706eb23f3968b24355d930d0e67f
LeylaMolnar <leyla.molnar24@gmail.com>
2023-02-16 15:44:21 +0200

    Universal header!
    Created the universal header and implemented it on:
    article.php
    blog.php
    cart.php
    shop.php

---
+++ partials/header.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/<?php echo $stylesheet; ?>.css">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="icon" href="images/Skatefavicon-16x16.png" type="image/x-icon">
+  <title><?php echo $title; ?></title>
+  <?php echo $extra;?>
+</head>
+
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg">
+      <div class="container-fluid my-class-with-padding">
+        <div class="cotainer-fluid img-1">
+          <a class="navbar-brand" href="index.php"></a>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 links">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="shop.php">Shop</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="blog.php">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#contacts">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php#about">About&nbsp;Us</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav mb-2 mb-lg-0 cart">
+            <li class="nav-item">
+              <a class="nav-link active" href="#"><img src="images/Icons/search.png" alt="search" width="30"
+                  height="30"></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link active" href="cart.php"><img src="images/Icons/Shoppingcart.png" alt="cart" width="30"
+                  height="30"></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
--- shop.php
+++ shop.php
@@ -1,69 +1,17 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css    ">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/shop.css">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-  <link rel="icon" href="images/Skatefavicon-16x16.png" type="image/x-icon">
-  <title>Skate Shop - Shop Page</title>
-</head>
-
-<body>
-  <header>
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid">
-        <div class="cotainer-fluid img-1">
-          <a class="navbar-brand" href="index.html"></a>
-        </div>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 links">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="shop.html" style="color: #000;">Shop</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="blog.html">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#contacts">Contact</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.html#about">About&nbsp;Us</a>
-            </li>
-          </ul>
-          <ul class="navbar-nav mb-2 mb-lg-0 cart">
-            <li class="nav-item">
-              <a class="nav-link active" href="#"><img src="images/Icons/search.png" alt="search" width="30"
-                  height="30"></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="cart.html"><img src="images/Icons/Shoppingcart.png" alt="cart" width="30"
-                  height="30"></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
+<?php
+$title = "Skate Shop - Shop Page";
+$stylesheet = "shop";
+$extra = "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css    \">";
+include "partials/header.php";
+?>
 
   <div class="container category">
     <div class="col-12 p-4 breadcrumbs">
-      <a href="index.html">
+      <a href="index.php">
         Main page
       </a>
       <img src="images/Icons/ArrowRight.png" alt="Blog3">
-      <a href="shop.html">
+      <a href="shop.php">
         <b>Shop</b>
       </a>
     </div>
@@ -135,7 +83,7 @@
     <div class="row">
       <div class="col d-flex justify-content-xl-start justify-content-center">
         <div class="card products">
-          <a href="product.html">
+          <a href="product.php">
             <img src="images/Product1.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">brand new sakteboard skull collection </h5>
@@ -149,7 +97,7 @@
 
       <div class="col d-flex justify-content-xl-start justify-content-center">
         <div class="card products">
-          <a href="product.html">
+          <a href="product.php">
             <img src="images/Product1.png" class=" card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">brand new sakteboard skull collection </h5>
@@ -163,7 +111,7 @@
 
       <div class="col d-flex justify-content-xl-start justify-content-center">
         <div class="card products">
-          <a href="product.html">
+          <a href="product.php">
             <img src="images/Product1.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">brand new sakteboard skull collection </h5>
@@ -177,7 +125,7 @@
 
       <div class="col d-flex justify-content-xl-start justify-content-center">
         <div class="card products">
-          <a href="product.html">
+          <a href="product.php">
             <img src="images/Product1.png" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">brand new sakteboard skull collection </h5>
