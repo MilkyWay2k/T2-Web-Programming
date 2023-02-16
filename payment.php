946f5f16a5b8f37f4df34b18f6eeb2c9f3edd604
LeylaMolnar <leyla.molnar24@gmail.com>
2023-02-16 16:05:39 +0200

    Universal header and footer 🌠everywhere 🌠
    Added universal header and footer to every page that was missing it

--- payment.php
+++ payment.php
@@ -1,60 +1,9 @@
-<!doctype html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="css/cart.css">
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-  <link rel="icon" href="images/Skatefavicon-16x16.png" type="image/x-icon">
-  <title>Skate Shop - Payment</title>
-</head>
-
-<body>
-  <header>
-    <nav class="navbar navbar-expand-lg">
-      <div class="container-fluid my-class-with-padding">
-        <div class="cotainer-fluid img-1">
-          <a class="navbar-brand" href="index.html"></a>
-        </div>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0 links">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="shop.html">Shop</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="blog.html">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#contacts">Contact</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php#about">About&nbsp;Us</a>
-            </li>
-          </ul>
-          <ul class="navbar-nav mb-2 mb-lg-0 cart">
-            <li class="nav-item">
-              <a class="nav-link active" href="#"><img src="images/Icons/search.png" alt="search" width="30"
-                  height="30"></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active" href="cart.html"><img src="images/Icons/Shoppingcart.png" alt="cart" width="30"
-                  height="30"></a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  </header>
+<?php
+$title = "Skate Shop - Payment";
+$stylesheet = "cart";
+$extra = "";
+include "partials/header.php";
+?>
 
   <div class="container">
     <div class="row">
@@ -123,7 +72,5 @@
 
 
   <?php
-
     include 'partials/footer.php';
-
   ?>
